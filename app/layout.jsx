e4595f8afad0663bbbb04d3d0720e66e95a1cb6c
Dubem <nwodochidubem1@gmail.com>
2023-06-16 08:23:54 -0400

    chore: file upload

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/global.css";
+
+export const metadata = {
+  title: "Promptia",
+  description: "Discover & Share AI Prompts",
+};
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            
+
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
